d81b136e6d44becd2eaeeef5477b35285910faf5
AndresGlz <andresgilgonzalez@outlook.com>
2023-09-28 12:51:41 -0600

    Agregar actividad 2

---
+++ U1-Actividad2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using U1_Actividad2.Models.ViewModels;
+
+namespace U1_Actividad2.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index(IndexViewModel vm)
+    {
+        if (vm.Conversion == null)
+        {
+            vm.Conversion = "MX";
+            return View(vm);
+        }
+
+        if (vm.Conversion == "MX")
+        {
+            vm.Resultado = vm.Cantidad / 18m;
+        }
+        else if (vm.Conversion == "USD") {
+            vm.Resultado = vm.Cantidad * 18m;
+        }
+
+        return View(vm);
+    }
+}
---
+++ U1-Actividad2/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace U1_Actividad2.Models.ViewModels;
+
+public class IndexViewModel
+{
+    public decimal Cantidad { get; set; }
+    public string Conversion { get; set; } = null!;
+    public decimal Resultado { get; set; }
+}
