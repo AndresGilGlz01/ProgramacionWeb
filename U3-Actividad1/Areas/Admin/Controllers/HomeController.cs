2d98b4f34f03b3e056acd3ffdf9da6342b931b3b
AndresGilGlz <andresgilgonzalez@outlook.com>
2023-10-24 15:50:31 -0600

    Admin area agregada

---
+++ U3-Actividad1/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace U3_Actividad1;
+
+[Area("Admin")]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
