d15fe056fab7e6ee1294a050050108a058477669
AndresGilGlz <andresgilgonzalez@outlook.com>
2023-10-30 18:52:43 -0600

    Proyecto creado

--- U3-Actividad1/Areas/Admin/Controllers/HomeController.cs
+++ U3-Actividad1/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using U3_Actividad1.Areas.Admin.Models;
+using U3_Actividad1.Repositories;
 
 namespace U3_Actividad1;
 
 [Area("Admin")]
 public class HomeController : Controller
 {
+    private VillancicoRepository _repository;
+
+    public HomeController(VillancicoRepository repository)
+    {
+        _repository = repository;
+    }
+
     public IActionResult Index()
     {
-        return View();
+        var viewModels = new AdminIndexViewModel()
+        {
+            Villancicos = _repository.Get()
+                .Select(v => new VillancicoModel()
+                {
+                    Id = v.Id,
+                    Nombre = v.Nombre
+                })
+        };
+
+        return View(viewModels);
     }
 }
---
+++ U3-Actividad1/Areas/Admin/Models/AdminIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace U3_Actividad1.Areas.Admin.Models;
+
+public class AdminIndexViewModel
+{
+    public IEnumerable<VillancicoModel> Villancicos = Enumerable.Empty<VillancicoModel>();
+}
+
+public class VillancicoModel 
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = null!;
+}
